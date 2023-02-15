454f7df5b44295c7fa92ef2136b4cfb59af13a77
Martín Mateos Sánchez <m.mateos97@gmail.com>
2023-02-15 23:15:25 +0100

    First Commit

---
+++ app/src/main/java/com/example/pruebapracticaandroid/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.pruebapracticaandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pruebapracticaandroid.ui.theme.PruebaPracticaAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PruebaPracticaAndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
